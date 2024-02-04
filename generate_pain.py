67ac8981339e1c9974f0092ce1645b7a846e220c
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2024-02-04 13:22:41 +0100

    Some more work done...

--- generate_pain.py
+++ generate_pain.py
@@ -32,13 +32,20 @@ NSMAP = {
     "xsi": "http://www.w3.org/2001/XMLSchema-instance",
 }
 
-def initg_pty(org_nbr):
-    initg_pty = etree.Element("InitgPty")
-    id_0 = etree.SubElement(initg_pty, "Id")
+
+def id_entry(org_nbr):
+    id_0 = etree.Element("Id")
     org_id = etree.SubElement(id_0, "OrgId")
     othr = etree.SubElement(org_id, "Othr")
     id_1 = etree.SubElement(othr, "Id")
     id_1.text = str(org_nbr)
+    return id_0
+
+
+def initg_pty(org_nbr):
+    initg_pty = etree.Element("InitgPty")
+    initg_pty.append(id_entry(org_nbr))
+    id_0 = etree.SubElement(initg_pty, "Id")
     return initg_pty
 
 
@@ -48,7 +55,67 @@ def payment(amount, ocr, date):
     pmt_inf_id.text = "Tmp text"  # FIXME
     pmt_mtd = etree.SubElement(pmt_inf, "PmtMtd")
     pmt_mtd.text = "TRF"
-    #<ReqdExctnDt>2020-10-31</ReqdExctnDt>
+    reqd_extn_dt = etree.SubElement(pmt_inf, "ReqdExctnDt")
+    reqd_extn_dt.text = date.isoformat()
+
+    dbtr = etree.SubElement(pmt_inf, "Dbtr")
+    nm = etree.SubElement(dbtr, "Nm")
+    nm.text = "Fixme"  # FIXME
+    dbtr.append(id_entry(888))  #FIXME org_nbr
+    ctry_of_res = etree.SubElement(dbtr, "CtryOfRes")
+    ctry_of_res.text = "SE"  # FIXME
+
+    dbtr_acct = etree.SubElement(pmt_inf, "DbtrAcct")
+    dbtr_acct_id = etree.SubElement(dbtr_acct, "Id")
+    dbtr_iban = etree.SubElement(dbtr_acct_id, "IBAN")
+    dbtr_iban.text = str(88)  # FIXME
+
+    dbtr_agt = etree.SubElement(pmt_inf, "DbtrAgt")
+    fin_inst_id = etree.SubElement(dbtr_agt, "FinInstnId")
+    bic = etree.SubElement(fin_inst_id, "BIC")
+    bic.text= "HANDSESS" # FIXME
+
+    cdt_trf_tx_inf = etree.SubElement(pmt_inf, "CdtTrfTxInf")
+    pmt_id = etree.SubElement(cdt_trf_tx_inf, "PmtId")
+    instr_id = etree.SubElement(pmt_id, "InstrId")
+    instr_id.text = "FIXME"  # FIXME
+    end_to_end_id = etree.SubElement(pmt_id, "EndToEndId")
+    end_to_end_id.text = "FIXME"  # FIXME
+
+    pmt_tp_inf = etree.SubElement(cdt_trf_tx_inf, "PmtTpInf")
+    svc_lvl = etree.SubElement(pmt_tp_inf, "SvcLvl")
+    etree.SubElement(svc_lvl, "Cd").text = "NURG"
+    ctgy_purp = etree.SubElement(pmt_tp_inf, "CtgyPurp")
+    etree.SubElement(ctgy_purp, "Cd").text = "SUPP"
+
+    amt = etree.SubElement(cdt_trf_tx_inf, "Amt")
+    etree.SubElement(amt, "InstdAmt", Ccy="SEK").text = str(f"{100:.2f}")  # FIXME
+
+    cdtr_agt = etree.SubElement(cdt_trf_tx_inf, "CdtrAgt")
+    fin_instn_id = etree.SubElement(cdtr_agt, "FinInstnId")
+    clr_sys_mmb_id = etree.SubElement(fin_instn_id, "ClrSysMmbId")
+    clr_sys_id = etree.SubElement(clr_sys_mmb_id, "ClrSysId")
+    etree.SubElement(clr_sys_id, "Cd").text = "SESBA"
+    etree.SubElement(clr_sys_mmb_id, "MmbId").text = str(9900)
+
+    cdtr = etree.SubElement(cdt_trf_tx_inf, "Cdtr")
+    etree.SubElement(cdtr, "Nm").text = "Mottagarnamn"  # FIXME
+
+    cdtr_acct = etree.SubElement(cdt_trf_tx_inf, "CdtrAcct")
+    cdtr_acct_id = etree.SubElement(cdtr_acct, "Id")
+    cdtr_acct_id_othr = etree.SubElement(cdtr_acct_id, "Othr")
+    etree.SubElement(cdtr_acct_id_othr, "Id").text = str(12345)  # FIXME: bankgiro!?!
+    schme_nm = etree.SubElement(cdtr_acct_id_othr, "SchmeNm")
+    etree.SubElement(schme_nm, "Prtry").text = "BGNR"
+
+    rmt_inf = etree.SubElement(cdt_trf_tx_inf, "RmtInf")
+    strd = etree.SubElement(rmt_inf, "Strd")
+    cdtr_ref_inf = etree.SubElement(strd, "CdtrRefInf")
+    tp = etree.SubElement(cdtr_ref_inf, "Tp")
+    cd_or_prtry = etree.SubElement(tp, "CdOrPrtry")
+    etree.SubElement(cd_or_prtry, "Cd").text = "SCOR"
+    etree.SubElement(cdtr_ref_inf, "Ref").text = str(1234)  # FIXME: OCR
+
     return pmt_inf
 
 
@@ -74,7 +141,7 @@ def _cli():
     cstmr_cdt_tr_initn = etree.SubElement(root, "CstmrCdtTrfInitn")
     cstmr_cdt_tr_initn.append(grp_hdr)
 
-    payment_0 = payment(500, 1234, "")
+    payment_0 = payment(500, 1234, datetime.date(2024, 2, 29))
     cstmr_cdt_tr_initn.append(payment_0)
 
     #output_filename = f"{args.input.stem}.xml"
